cb1cbbca1c76017430106e59b5fd8ca07df99ba1
ech0m4i <ech0m4i@icloud.com>
2020-09-29 15:52:34 +0800

    Got Heap Optimized.

--- priority_queue/HeapOpt.java
+++ priority_queue/HeapOpt.java
@@ -4,7 +4,10 @@ import algs4.*;
 import java.util.Arrays;
 public class HeapOpt {
 	private static int cnt;
-	private static void sort(int[] a) {
+	private static int cntresn;
+	private static int cntreso;
+	private static void sorto(int[] a) {
+		cntreso=0;
 		cnt=0;
 		int n=a.length;
 		for(int i=n/2;i>=1;i--) sink(a,i,n);//1~n/2
@@ -14,6 +17,18 @@ public class HeapOpt {
 			exch(a,1,n--);
 			sinkwithswim(a,1,n);
 		}
+		cntreso=cnt;
+	}
+	private static void sortn(int[] a) {
+		cntresn=0;
+		cnt=0;
+		int n=a.length;
+		for(int i=n/2;i>=1;i--) sink(a,i,n);
+		while(n>1) {
+			exch(a,1,n--);
+			sink(a,1,n);
+		}
+		cntresn=cnt;
 	}
 	//heap build.
 	private static void sink(int[] a,int i,int n) {
@@ -45,9 +60,10 @@ public class HeapOpt {
 	}
 	private static void exch(int[] a,int i,int j) {
 		int temp=a[i-1];a[i-1]=a[j-1];a[j-1]=temp;
-		++cnt;
+		//++cnt;//damn it this is not less() operation...
 	}
 	private static boolean less(int[] a,int v,int w) {
+		++cnt;
 		return a[v-1]<a[w-1];
 	}
 	private static boolean sorted(int[] a) {
@@ -57,16 +73,29 @@ public class HeapOpt {
 	}
 	public static void main(String[] args) {
 		int x=StdIn.readInt();
-		int alen=(int)Math.pow(2,x);
+		int alen=(int)Math.pow(10,x);
+		int[] ac;
 		int[] a=
 		//{0,3,7,7,6,9,2,0,5,10,3,7,7,6,9,2,0,5,1};
 		//{8, 1, 8, 4, 17, 2, 19, 2, 10, 6, 3, 14, 9, 9, 1, 1};
 		new int[alen];
-		for(int i=0;i<alen;i++) a[i]=StdRandom.uniform(0,100);
+		for(int i=0;i<alen;i++) a[i]=
+			//alen-i;
+		//StdRandom.shuffle(a);
+			StdRandom.uniform(0,10000);
 		//StdOut.println(Arrays.toString(a));
-		sort(a);
-		StdOut.println(Math.log(cnt)/Math.log(2));
+		//ac=a.clone();
+		sortn(a);
+		//StdRandom.shuffle(a);
+		for(int i=0;i<alen;i++) StdRandom.uniform(0,10000);
+		sorto(a);
+		//StdOut.println(Math.log(cnt)/Math.log(2));
 		//StdOut.println("result: "+Arrays.toString(a));
-		assert sorted(a);
+		//assert sorted(a);
+		StdOut.println(Math.log(cntresn)/Math.log(2)+" "+
+					Math.log(cntreso)/Math.log(2)+"\n"+
+					cntresn+" "+cntreso+" "+(double)cntresn/(cntreso*2));
+		//maybe something wrong, not optimized enough to half exch()...
+		//oops it's really allright, just get the less() times not exch()...
 	}
 }
